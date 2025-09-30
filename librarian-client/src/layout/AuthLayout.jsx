528d11ddea8ac54b3296a5a486dc9709508f876c
faturrahman04 <faturrahmanalfarisi449@gmail.com>
2025-09-30 22:56:44 +0700

    style(className): Ubah layout menjadi full

--- librarian-client/src/layout/AuthLayout.jsx
+++ librarian-client/src/layout/AuthLayout.jsx
@@ -5,7 +5,7 @@ const AuthLayout = () => {
   const location = useLocation();
   const auth = location.pathname == '/login'
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center h-[100dvh]">
       <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
         { auth ? 'Login' : 'Sign up' }
       </h2>
